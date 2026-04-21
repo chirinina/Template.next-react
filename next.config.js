3a6caf23c4248e5191f5c9e25dc5ab4cd8852992
Efrain Chir Nina <jherychirinina@gmail.com>
2026-04-21 17:34:16 -0400

    Inicio1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true // Habilitar el modo estricto para React
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
